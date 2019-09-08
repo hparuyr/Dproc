13c4b57434bcd36253321ac47bcd3d07e24a52bf
Narek <narekmalkhasyan1@gmail.com>
2019-09-09 00:49:41 +0400

    Minor changes, deletion of commented code

--- src/main/java/am/dproc/sms/rest/SurveyController.java
+++ src/main/java/am/dproc/sms/rest/SurveyController.java
@@ -48,7 +48,6 @@ public class SurveyController {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	// initializes survey by requesting questions list, parsing body of response and saving questions list in db 
 	@Bean(initMethod = "initSurvey")
 	public void initSurvey() throws IOException {
 		if(getSurvey().size() > 0) {
@@ -58,28 +57,22 @@ public class SurveyController {
 		saveSurveyQuestions(response.getBody());
 	}
 	
-	// parses response body and saves survey questions(50) to db
 	private int[] saveSurveyQuestions(String body) throws IOException{
 		Document doc = Jsoup.parse(body);
-		System.out.println("--->Title: "+doc.title());
 		Elements newsHeadlines = doc.select("form table tbody tr");
-		int count = 0;
 		List<SurveyQuestion> surveyList = new ArrayList<>();
 		for (Element headline : newsHeadlines) {
 			String questionContent = headline.child(0).text();
 			if(questionContent.length() < 1) {
 				continue;
 			}
-		  System.out.println(++count+"."+questionContent);
 		  if(headline.children().size() > 2) {
 			  Element childData = headline.child(1);
 			  if(childData.children().size() > 1) {
-				  Element firstChildInput = headline.child(1).child(0);
-				  Element seconfChildInput = headline.child(1).child(1);
-				  System.out.println("second child->> name: "+firstChildInput.attr("name")+" name: "+seconfChildInput.attr("name"));
+				  Element secondChildInput = headline.child(1).child(1);
 				  SurveyQuestion survey = new SurveyQuestion();
 				  survey.setContent(questionContent);
-				  survey.setInputName(seconfChildInput.attr("name"));
+				  survey.setInputName(secondChildInput.attr("name"));
 				  surveyList.add(survey);
 			  }
 		  }
@@ -93,36 +86,7 @@ public class SurveyController {
 	public List<SurveyQuestion> getSurvey() {
 		return surveyQuestionService.getAll();
 	}
-	
-//	@Path("/submit")
-//	@POST
-//	@Produces(MediaType.APPLICATION_JSON_VALUE)
-//	@Consumes(MediaType.APPLICATION_JSON_VALUE)
-//	public ResponseEntity<String> submitSurvey(List<SurveyQuestion> surveyQuestions) {
-//		String host = "https://openpsychometrics.org";
-//		String submitUrl = "https://openpsychometrics.org/tests/IPIP-BFFM/2.php";
-//
-//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//		params.add("unqid", "5d70b53b1fe591078232897");
-//		params.add("seconds", Integer.toString((int)(System.currentTimeMillis()/1000)));
-//		for (SurveyQuestion surveyQuestion : surveyQuestions) {
-//			int randomSec = (int) (Math.random() * 1000 + 1000);
-//			int randomVal = (int) (Math.random() * 5);
-//			params.add(surveyQuestion.getInputName()+"_E", String.valueOf(randomSec));
-//			params.add(surveyQuestion.getInputName(), String.valueOf(randomVal));
-//		}
-//		params.add("onblurs", "0");
-//
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//		headers.setCacheControl("max-age=0");
-//		headers.setOrigin(host);
-//
-//		ResponseEntity<String> response = doPostRequest(submitUrl, params, headers);
-//		return response;
-//	}
 
-	// submits survey questions by input names and get response containg scores
 	private ResponseEntity<String> submitSurvey(Map<String, String> answers) {
 		String host = "https://openpsychometrics.org";
 		String submitUrl = "https://openpsychometrics.org/tests/IPIP-BFFM/2.php";
@@ -148,7 +112,6 @@ public class SurveyController {
 		return response;
 	}
 
-	// requesting page containing questions list and get repsonse
 	private ResponseEntity<String> getQuestionsPage() {
 		String host = "https://openpsychometrics.org";
 		String submitUrl = "https://openpsychometrics.org/tests/IPIP-BFFM/1.php";
@@ -166,14 +129,11 @@ public class SurveyController {
 		return response;
 	}
 	
-	// do post request with url, params and headers
 	private ResponseEntity<String> doPostRequest(String url, MultiValueMap<String, String> params, HttpHeaders headers){
 		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params,headers);
-		System.out.println(entity.toString());
 		return restTemplate.postForEntity(url, entity, String.class);
 	}
 	
-	// get survey result by student id
 	@Path("/result/{studentId}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON_VALUE)
@@ -181,7 +141,6 @@ public class SurveyController {
 		return surveyResultService.getByStudentId(studentId);
 	}
 	
-	// get all survey results
 	@Path("/result")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON_VALUE)
@@ -206,7 +165,6 @@ public class SurveyController {
 		return surveyResultService.deleteAll();
 	}
 	
-	// get scores from response html
 	private SurveyResult getResponseScores(ResponseEntity<String> response) {
 		Document doc = Jsoup.parse(response.getBody());
 		
@@ -221,7 +179,6 @@ public class SurveyController {
 		return surveyResult;
 	}
 
-	// submits answers, gets response, parses and saves score values to db
 	@Path("/submit")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON_VALUE)
